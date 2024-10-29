b550cec11c4ceb7c2480b05222b8b34b8f88286f
Ken Broden <brod0268@morris.umn.edu>
2024-10-28 20:56:14 -0500

    Created server

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,58 @@
 package echoserver;
 
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class EchoServer {
+        
+        public static void main(String[] args) {
+            
+            ServerSocket serverSocket = null;
+            Socket socket = null;
+            InputStreamReader inputStreamReader = null;
+            OutputStream outputStream = null;
+            BufferedReader bufferedReader = null;
+            BufferedWriter bufferedWriter = null;
+            
+            serverSocket = new ServerSocket(8080);
+
+            while (true) {
+
+                try {
+
+                    socket = serverSocket.accept();
+
+                    inputStreamReader = new InputStreamReader(socket.getInputStream());
+                    outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+
+                    bufferedReader = new BufferedReader(inputStreamReader);
+                    bufferedWriter = new BufferedWriter(outputStreamWriter);
+                    
+                    while (true) {
+
+                        String msgReceived = bufferedReader.readLine();
+
+                        System.out.println("Client: " + msgFromClient);
+
+                        bufferedWriter.write("MSG Received.");
+                        bufferedWriter.newLine();
+                        bufferedWriter.flush();
+
+                        if (msgReceived.equals("exit")) {
+                            break;
+                        }
+
+                socket.close();
+                inputStreamReader.close();
+                outputStream.close();
+                bufferedReader.close();
+                bufferedWriter.close();
+                }
+
+            }
+                
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 }
